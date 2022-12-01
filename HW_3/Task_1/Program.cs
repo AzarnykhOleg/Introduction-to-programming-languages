89abc1234fb05c46a08a61a1cd440322ecc7d1b0
Oleg <olazar61@gmail.com>
2022-12-02 02:35:30 +0300

    Homework_3

---
+++ HW_3/Task_1/Program.cs
@@ -0,0 +1,57 @@
+﻿/*/* 
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+Задание должно быть выполнено в методе. В методе не должно быть вывода в консоль. Не использовать String!
+14212 -> нет
+12821 -> да
+23432 -> да
+ */
+int num = InputNum();
+Console.Write(IsPalindrome(num));
+
+
+
+int InputNum()
+{
+    Console.Write("Введите пятизначное число => ");
+    string text = Console.ReadLine();
+    if (!int.TryParse(text, out int number))
+    {
+        Console.WriteLine("Вы ввели НЕ число!", number);
+        return -1;
+    }
+    else
+    {
+        int num = Convert.ToInt32(text);
+        if (num / 100000 >= 1 | num / 10000 < 1)
+        {
+            Console.WriteLine("Введено НЕ пятизначное число!");
+            return -1;
+        }
+        else
+        {
+            return num;
+        }
+    }
+}
+
+
+string IsPalindrome(int num)
+{
+    int num_1 = num / 10000;
+    int num_2 = (num - num_1 * 10000) / 1000;
+    int num_4 = (num / 10) % 10;
+    int num_5 = num % 10;
+    if (num == -1)
+    {
+        return ($"Ошибка ввода.");
+    }
+    else if (num_1 == num_5 & num_2 == num_4)
+    {
+        return ($"Число {num} - палиндром.");
+    }
+    else
+    {
+        return ($"Число {num} - НЕ палиндром.");
+    }
+}
+
---
+++ HW_3/Task_2/Program.cs
@@ -0,0 +1,55 @@
+﻿/* 
+Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве. 
+Задание должно быть выполнено в методе. В методе не должно быть вывода в консоль.
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53
+ */
+
+Console.WriteLine("Введите трёхмерные координаты первой точки.");
+double[] first = InputCoordinates();
+Console.WriteLine("Введите трёхмерные координаты второй точки.");
+double[] second = InputCoordinates();
+double distances = distance(first, second);
+Console.WriteLine($"Расстояние между точками равно {distances}");
+
+
+
+// Метод для валидации вводимых пользователем значений каждой из координат.
+double InputNum()
+{
+    string text = Console.ReadLine();
+    if (!double.TryParse(text, out double number))
+    {
+        Console.WriteLine("Вы ввели НЕ число!", number);
+        return -1;
+    }
+    else
+    {
+        double num = Convert.ToDouble(text);
+        return num;
+    }
+}
+
+// Метод для ввода трёхмерных координат.
+double[] InputCoordinates()
+{
+    double[] result = new double[3];
+    for (int i = 0; i < 3; i++)
+    {
+        double num = InputNum();
+        result[i] = num;
+    }
+    return result;
+}
+
+// Метод для расчёта расстояния между точками.
+double distance(double[] first, double[] second)
+{
+    double distance = Math.Round(Math.Sqrt(Math.Pow(second[0] - first[0], 2) +
+                                           Math.Pow(second[1] - first[1], 2) +
+                                           Math.Pow(second[2] - first[2], 2)), 2);
+    return distance;
+}
+
+
+
